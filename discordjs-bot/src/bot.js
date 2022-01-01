bd68dd30dc412dbf895a60a46274a5a68e60b3fd
Louis Castillo <87865123+castillosdev@users.noreply.github.com>
2022-01-01 16:53:58 -0500

    Adding comands to the bot

--- discordjs-bot/src/bot.js
+++ discordjs-bot/src/bot.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const PREFIX = "$";
 
 client.on('ready', () => {
     console.log( `${client.user.tag} has logged in.`);
@@ -9,11 +10,21 @@ client.on('ready', () => {
 
 client.on('messageCreate', (message) => {
     if (message.author.bot) return;
-    console.log(`[${message.author.tag}]: ${message.content}`);
-    if (message.content === 'hello') {
-        //message.reply('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
-        message.channel.send('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
+    //console.log(`[${message.author.tag}]: ${message.content}`);
+   /* if (message.content === 'hello') {
+        message.reply('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
+        //message.channel.send('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
+    }*/
+
+    if (message.content.startsWith(PREFIX)) { 
+        const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(" ");
+        console.log(CMD_NAME);
+        console.log(args)
     }
+
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN)
