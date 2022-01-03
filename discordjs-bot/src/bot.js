830e403a326808a46402e124f7eb58d7fd5636d8
Louis Castillo <87865123+castillosdev@users.noreply.github.com>
2022-01-03 00:51:06 -0500

    Adding the kick functionality....figure out the permissions checker

--- discordjs-bot/src/bot.js
+++ discordjs-bot/src/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const PREFIX = "$";
 
@@ -9,20 +9,36 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-    if (message.author.bot) return;
-    //console.log(`[${message.author.tag}]: ${message.content}`);
-   /* if (message.content === 'hello') {
-        message.reply('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
-        //message.channel.send('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
-    }*/
+        if (message.author.bot) return;
+        //console.log(`[${message.author.tag}]: ${message.content}`);
+    /* if (message.content === 'hello') {
+            message.reply('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
+            //message.channel.send('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
+        }*/
 
-    if (message.content.startsWith(PREFIX)) { 
-        const [CMD_NAME, ...args] = message.content
-        .trim()
-        .substring(PREFIX.length)
-        .split(" ");
-        console.log(CMD_NAME);
-        console.log(args)
+        if (message.content.startsWith(PREFIX)) { 
+            const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+
+        if (CMD_NAME === 'kick') {
+            if (!message.member.hasPermission('KICK_MEMBERS'))
+                return message.reply('You do not have permissions to use that command');
+            if (args.length === 0)
+                return message.reply('Please provide the ID of the offender.');
+            const member = message.guild.members.cache.get(args[0]);
+            if (member) {
+                member
+                .kick()
+                .then((member) => message.channel.send(`${member} was kicked.`))
+                .catch((err) => message.channel.send('I do not have permissions :( '));
+            } else {
+                message.channel.send('That Member was not found');
+            }
+
+
+        }
     }
 
 });
