e21ace452bc58966eeb69e93d8501304b5da56dc
Louis Castillo <87865123+castillosdev@users.noreply.github.com>
2022-01-11 01:29:58 -0500

    Finished setting up basinc commands. Installed all dependencies for music streaming functionality.

--- discordjs-bot/src/bot.js
+++ discordjs-bot/src/bot.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
-const { Client, Intents, GuildMember, ReactionEmoji } = require('discord.js');
+const { Client, Intents, WebhookClient, GuildMember, ReactionEmoji } = require('discord.js');
 const client = new Client( 
   {
   partials: ['MESSAGE' , 'CHANNEL', 'REACTON'],
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
 });
+
+const webhookClient = new WebhookClient({
+id: '930334808859611136', token: '0IwhxTbeb7YAV_7sblTDPKo_wwmaZv2uFiyQ-9xZNadzjS3wIBufuL9T15lETkfnDDwk'
+}
+  );
+
 const PREFIX = '$';
 
 client.on('ready', () => {
@@ -14,11 +20,11 @@ client.on('ready', () => {
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
-  //console.log(`[${message.author.tag}]: ${message.content}`);
-  /* if (message.content === 'hello') {
+  console.log(`[${message.author.tag}]: ${message.content}`);
+    if (message.content === 'hello') {
             message.reply('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
             //message.channel.send('https://media.giphy.com/media/KtnNLZVycYLdutzEm9/giphy.gif');
-        }*/
+        }
 
   if (message.content.startsWith(PREFIX)) {
     const [CMD_NAME, ...args] = message.content
@@ -57,6 +63,11 @@ client.on('messageCreate', async (message) => {
         console.log(err);
         message.channel.send('An error occured. Either I do not have permission or the user was not found.')
       }
+    }else if (CMD_NAME === 'announce') {
+      console.log(args);
+      const msg = args.join(' ');
+      console.log(msg);
+      webhookClient.send(msg);
     }
   }
 });
